a0394d4d2599225d8762fc5abbbd47f8f0f47fae
waterdog05 <84177301+waterdog05@users.noreply.github.com>
2021-10-16 06:56:09 +0900

    Add files via upload

---
+++ sketch_oct15a.ino
@@ -0,0 +1,82 @@
+#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
+
+Servo servo1;  //servo setup
+Servo servo2;
+int pos1 = 90;
+int pos2 = 90;
+
+int trigPin1 = 4;  //trig, echo setup
+int echoPin1 = 5;
+int trigPin2 = 6;
+int echoPin2 = 7;
+
+const int buttonPin1 = 8;  //1p button setup
+int buttonState1 = 0;
+int lastButtonState1 = 0;
+int buttonPushCounter1;
+
+const int buttonPin2 = 10;  //2p button setup
+int buttonState2 = 0;
+int lastButtonState2 = 0;
+int buttonPushCounter2;
+
+int btnCnt1();  //button counting function
+int btnCnt2();
+void showResult();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  servo1.attach(2);
+  servo2.attach(3);
+  
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:  
+  buttonPushCounter1 = btnCnt1();  //count push
+  buttonPushCounter2 = btnCnt2();
+  Serial.print("buttonPushCounter1 : ");  //print push
+  Serial.println(buttonPushCounter1);
+  Serial.print("buttonPushCounter2 : ");
+  Serial.println(buttonPushCounter2);
+  Serial.println();
+  delay(50);
+}
+
+int btnCnt1() {
+  buttonState1 = digitalRead(buttonPin1);
+  
+  if (buttonState1 != lastButtonState1) {
+    if (buttonState1 == HIGH) {
+      buttonPushCounter1++;
+    }
+    delay(50);
+  }
+  lastButtonState1 = buttonState1;
+
+  return buttonPushCounter1;
+}
+
+int btnCnt2() {
+  buttonState2 = digitalRead(buttonPin2);
+  
+  if (buttonState2 != lastButtonState2) {
+    if (buttonState2 == HIGH) {
+      buttonPushCounter2++;
+    }
+    delay(50);
+  }
+  lastButtonState2 = buttonState2;
+
+  return buttonPushCounter2;
+}

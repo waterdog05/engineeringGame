17005652343ad0d8faedcbab724ca9af3b30ee3d
waterdog05 <84177301+waterdog05@users.noreply.github.com>
2021-12-27 23:24:43 +0900

    Update sketch_engineeringGame_V2.ino

--- sketch_engineeringGame_V2.ino
+++ sketch_engineeringGame_V2.ino
@@ -2,73 +2,73 @@
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
 
-Servo servo1;  //servo setup
+Servo servo1;  //서보모터 객체 선언
 Servo servo2;
 const int servo1Pin = 2;
 const int servo2Pin = 3;
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); //SCL = A4; SDA = A5;
+LiquidCrystal_I2C lcd(0x27, 20, 4); //lcd 스크린 설정
 
-const int startButtonPin = 8;  //starting button setup
+const int startButtonPin = 8;  //시작 버튼 설정
 int startButtonState = LOW;
 
-int firstp_Led = 4;  //Led setup
+int firstp_Led = 4;  //Led 설정
 int secondp_Led = 6;
 
-const int buttonPin1 = 10;  //1p button setup
+const int buttonPin1 = 10;  //1p 버튼 설정
 int buttonState1 = 0;
 int lastButtonState1 = 0;
 int buttonPushCounter1 = 0;
 
-const int buttonPin2 = 12;  //2p button setup
+const int buttonPin2 = 12;  //2p 버튼 설정
 int buttonState2 = 0;
 int lastButtonState2 = 0;
 int buttonPushCounter2 = 0;
 
-int finWinner = 0;  //final winner
-int cntFirstPWin = 0;  //for calculating final winner
+int finWinner = 0;  //최종 우승자
+int cntFirstPWin = 0;  //최종우승자 결정위한 값 저장
 int cntSecondPWin = 0;
-int stopper = 0;  //break inner for(;;)
-int i = 0;  //break outer for(;;)
+int stopper = 0;  //내부 무한루프 탈출용
+int i = 0;  //외부 무한루프 탈출용
 
-void gameStarting();  //setting game
-void btnCnt1();  //button counting function
+void gameStarting();  //게임 셋업 함수
+void btnCnt1();  //버튼 누른 횟수 세는 함수
 void btnCnt2();
-int stopCountPush();  //function that ends game after 1m for MsTimer2
-void moveBelt();  //move belt during game
-void calcFinWinner();  //choose final winner
-void ShowResult();  //show game result
-void gameReset();  //reset game
+int stopCountPush();  //stopper 값 변경 함수
+void moveBelt();  //모터 구동 함수
+void calcFinWinner();  //최종 우승자 결정 함수
+void ShowResult();  //게임 결과 출력 함수
+void gameReset();  //게임 리셋 함수
 
 void setup() {
   Serial.begin(9600);
 
-  lcd.begin();  //lcd setup
+  lcd.begin();
   lcd.backlight();
 
-  servo1.attach(servo1Pin);  //servo setup
+  servo1.attach(servo1Pin); 
   servo2.attach(servo2Pin);
   
-  pinMode(buttonPin1, INPUT);  //buttonPin setup
+  pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(startButtonPin, INPUT);
 
-  pinMode(firstp_Led, OUTPUT);  //LedPin setup
+  pinMode(firstp_Led, OUTPUT);
   pinMode(secondp_Led, OUTPUT);
 
-  MsTimer2::set(1000, stopCountPush);  //game play time = 20sec
+  MsTimer2::set(1000, stopCountPush);  //playing time = 20sec
 }
 
 void loop() {
-  startButtonState = digitalRead(startButtonPin);  //If button is pressed
+  startButtonState = digitalRead(startButtonPin);
   if (startButtonState == HIGH) {
-    gameStarting();  //start game
+    gameStarting();
     
     for (;;) {
       lcd.setCursor(2, 0);
       lcd.print("Engineering Game");
       
-      MsTimer2::start();  //start Timer to break for(;;) in line 72
+      MsTimer2::start();
       for (;;) {
         btnCnt1();
         btnCnt2();
@@ -78,34 +78,34 @@ void loop() {
           break;
         }
       }
-      moveBelt();  //move belt with the stored num
+      moveBelt();
       lcd.clear();
       lcd.setCursor(2, 0);
       lcd.print("Engineering Game");
       stopper = 0;
       i++;
       
-      if (i == 20) {  //set i to break for(;;) in line 67
+      if (i == 20) {
         i = 0;
         break;
       }
     }
     
-    calcFinWinner();  //calculate final winner
+    calcFinWinner();
     delay(500);
-    showResult();  //end game and show result
-    gameReset();  //reset game
+    showResult();
+    gameReset();
   }
 }
 
 void gameStarting() {
   lcd.clear();
   lcd.setCursor(2, 1);
-  lcd.print("Engineering Game");  //Engineering Game
+  lcd.print("Engineering Game");
   delay(2000);
   lcd.clear();
   
-  lcd.setCursor(9, 1);  //3 2 1
+  lcd.setCursor(9, 1);
   lcd.print(3);
   delay(1000);
   lcd.print(2);
@@ -113,14 +113,14 @@ void gameStarting() {
   lcd.print(1);
   delay(1000);
   
-  lcd.setCursor(7, 1);  //start!
+  lcd.setCursor(7, 1);
   lcd.print("start!");
   delay(1000);
-  lcd.clear();  //7sec
+  lcd.clear();
 }
 
 void btnCnt1() {
-  buttonState1 = digitalRead(buttonPin1);  //count 1P's btn push
+  buttonState1 = digitalRead(buttonPin1);
   
   if (buttonState1 != lastButtonState1) {
     if (buttonState1 == HIGH) {
@@ -130,14 +130,14 @@ void btnCnt1() {
   }
   lastButtonState1 = buttonState1;
 
-  lcd.setCursor(1, 2);  //print 1P's push count on lcd
+  lcd.setCursor(1, 2);
   lcd.print("1P:");
   lcd.setCursor(4, 2);
   lcd.print(buttonPushCounter1);
 }
 
 void btnCnt2() {  
-  buttonState2 = digitalRead(buttonPin2);  //count 2P's btn push
+  buttonState2 = digitalRead(buttonPin2);
   
   if (buttonState2 != lastButtonState2) {
     if (buttonState2 == HIGH) {
@@ -147,7 +147,7 @@ void btnCnt2() {
   }
   lastButtonState2 = buttonState2;
   
-  lcd.setCursor(14, 2);  //print 2P's push count on lcd
+  lcd.setCursor(14, 2);
   lcd.print("2P:");
   lcd.setCursor(17, 2);
   lcd.print(buttonPushCounter2);
@@ -177,7 +177,7 @@ void moveBelt() {
     servo2.write(90);
   }
   else {
-    cntFirstPWin = cntFirstPWin;  //tie
+    cntFirstPWin = cntFirstPWin;  //1P == 2P
     cntSecondPWin = cntSecondPWin;
   }
 }
@@ -189,7 +189,7 @@ void calcFinWinner() {
   else if (cntFirstPWin < cntSecondPWin) {  //2P win
     finWinner = 2;
   }
-  else {  //tie
+  else {  //1P == 2P
     finWinner = 3;
   }
 }
@@ -254,7 +254,7 @@ void gameReset() {
   lcd.setCursor(2, 1);
   lcd.print("Game Resetting..");
   
-  switch (finWinner) {  //reset belt condition
+  switch (finWinner) {  //reset belt position
     case 1:
       finWinner = 0;
 
